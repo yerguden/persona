a0a09685ac6d1550de2c1b13b5628968be03787a
yuko <yerguden@gmail.com>
2025-01-05 15:01:10 +0100

    add s3 dependencies

--- config/runtime.exs
+++ config/runtime.exs
@@ -65,6 +65,17 @@ if config_env() == :prod do
     ],
     secret_key_base: secret_key_base
 
+  config :ex_aws,
+    debug_requests: true,
+    json_codec: Jason,
+    access_key_id: {:system, "AWS_ACCESS_KEY_ID"},
+    secret_access_key: {:system, "AWS_SECRET_ACCESS_KEY"}
+
+  config :ex_aws, :s3,
+    scheme: "https://",
+    host: "fly.storage.tigris.dev",
+    region: "auto"
+
   # ## SSL Support
   #
   # To get SSL working, you will need to add the `https` key
--- mix.exs
+++ mix.exs
@@ -57,7 +57,12 @@ defmodule Persona.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:ex_aws, "~> 2.0"},
+      {:ex_aws_s3, "~> 2.0"},
+      {:poison, "~> 3.0"},
+      {:hackney, "~> 1.9"},
+      {:sweet_xml, "~> 0.6.6"}
     ]
   end
 
