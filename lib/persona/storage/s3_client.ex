8c7bacf967807ffb633415ae8d00bd78fb23e9be
yuko <yerguden@gmail.com>
2025-02-04 23:50:43 +0100

    Defined download_file callback for s3_client

--- lib/persona/storage/s3_client.ex
+++ lib/persona/storage/s3_client.ex
@@ -6,9 +6,15 @@ defmodule Persona.Storage.S3Client do
               opts :: keyword()
             ) ::
               {:ok, String.t()} | {:error, any()}
+  @callback download_file(s3_key :: String.t()) ::
+              {:ok, Stream.t()} | {:error, any()}
 
   def presigned_url(operation, bucket, key, opts),
     do: impl().presigned_url(operation, bucket, key, opts)
+
+  def download_file(s3_key) do
+    impl().download_file(s3_key)
+  end
 
   defp impl, do: Application.get_env(:persona, :s3_client, Persona.Storage.S3)
 end
@@ -19,5 +25,9 @@ defmodule Persona.Storage.S3 do
   def presigned_url(operation, bucket, key, opts) do
     ExAws.Config.new(:s3)
     |> ExAws.S3.presigned_url(operation, bucket, key, opts)
+  end
+
+  def download_file(s3_key) do
+    {:error, :todo}
   end
 end
